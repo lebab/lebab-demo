0af90d09dc69bdf1d19798400ca132cc9bb87a3a
Rene Saarsoo <nene@triin.net>
2023-03-11 21:22:17 +0200

    Display two editors side-by-side

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import type { Component } from "solid-js";
 import { Editor } from "./Editor";
+import styles from "./App.module.css";
 
 export const App: Component = () => {
   return (
-    <>
+    <div class={styles.App}>
       <h1>Hello Lebab!</h1>
       <Editor />
-    </>
+      <Editor />
+    </div>
   );
 };
