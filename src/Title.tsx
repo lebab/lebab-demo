b2d0151f4891e36af55202b34a2a80532e21031d
Rene Saarsoo <nene@triin.net>
2023-03-12 16:58:12 +0200

    Link from title to github page

--- src/Title.tsx
+++ src/Title.tsx
@@ -3,7 +3,9 @@ import styles from "./Title.module.css";
 
 export const Title: Component = () => (
   <div class={styles.Title}>
-    <h1>Lebab</h1>
+    <h1>
+      <a href="https://github.com/lebab/lebab/">Lebab</a>
+    </h1>
     <p>Lebab modernizes your JavaScript code!</p>
   </div>
 );
